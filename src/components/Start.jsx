e55af0c2111ccc7e5fec8bc93403b363ecf5a9d7
SaraWallener <69631195+SaraWallener@users.noreply.github.com>
2021-02-24 14:27:23 +0100

    Landing page done

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+import '../css/start.css';
+import airbean from '../assets/graphics/airbean-landing.svg';
+import introleft from '../assets/graphics/intro-graphic-left.svg';
+import introright from '../assets/graphics/intro-graphic-right.svg';
 
 
 export default function Start(){
+const history = useHistory();
+
+function clickHandler(){
+    history.push('/menu');
+}
+
     return (
-        <div>
-            <h1>START PAGE</h1>
+        <div className="landing-page">
+        <div className="img-content">
+            <img src={introleft}></img>
+            <div className="logo">
+            <img src={airbean}></img>
+            </div>
+            <div className="img-right">
+            <img src={introright} onClick={clickHandler} ></img>
+            </div>
         </div>
+        </div>
+
     )
 }
