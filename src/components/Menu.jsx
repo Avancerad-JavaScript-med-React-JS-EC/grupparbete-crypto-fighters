a4677e8c38fc962e3c2c40c4649eb0c5faa11786
Traiq <mr.tm84@gmail.com>
2021-02-24 15:48:21 +0100

    fetch menu products

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
-import Products from './Products';
+import React , { useState, useEffect } from 'react';
+import Product from './Product'
 
-export default function Menu(){
-    return (
-        <div><Products /></div>
-    )
+
+
+const Menu = () => {
+
+ 
+  const [products, setProducts] = useState([]);
+
+// UseEffect hook to fetch the content from the source
+  useEffect( () => {
+    async function fetchProducts(){
+    const response = await fetch('http://localhost:5000/api/beans');
+    const data = await response.json();
+    console.log(data)
+
+    setProducts(data.menu);
+  
+    }
+   
+    fetchProducts();
+  }, []);
+
+  
+  return (
+    <div className="App">
+    <h1>Meny</h1>
+    { products.map((product) => {
+        return <Product product={ product } key={ product.id }  />
+    })}
+    </div>
+  );
+  
 }
+
+export default Menu;
--- src/components/Product.js
+++ src/components/Product.js
@@ -5,11 +5,11 @@ const Product = ({ product }) => {
   return (
     // Clickable Div for choosing desired product 
     <div className="product-grid">
-      <h3 className="date g-left"> {product.title} </h3>
+      <h3 className="title g-left"> {product.title} </h3>
       <div className="g-center">
-        <h3 className="name"> {product.desc} </h3>
+        <h3 className="desc"> {product.desc} </h3>
       </div>
-      <h4 className="cost g-right">{product.price} sek</h4>
+      <h4 className="price g-right">{product.price} sek</h4>
     </div>
   );
 };
