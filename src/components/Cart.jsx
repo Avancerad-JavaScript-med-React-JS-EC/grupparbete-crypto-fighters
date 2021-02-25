9f57e73cc0c7be6f8770d61ea06cb47ca4ba8d06
Traiq <mr.tm84@gmail.com>
2021-02-25 17:15:57 +0100

    Cart menu

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import '../css/navCart.css';
 import { useSelector } from 'react-redux';
 
-export default function Cart(){
+export default function Cart(props){
+
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = (e) => setIsOpen(!isOpen);
     
 
+
     return (
-        <div>
-            <button className="navCart"><div className="navCartIcon"></div></button>
+        <div className="cart-wrap">
+            <button className="navCart" onClick={toggle}><div className="navCartIcon"><span className="cartCount">{props.selectedItems}</span></div>
+            </button>
+            <div className={`popup-menu ${isOpen ? 'shown' : ' '}`} ><div>menu</div><button>Pay</button></div>
         </div>
     )
 }
