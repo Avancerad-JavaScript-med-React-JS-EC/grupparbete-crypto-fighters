ab164ad48730bebfda54de276e39bcd207acf581
Traiq <mr.tm84@gmail.com>
2021-02-26 18:08:57 +0100

    Adding items to cart (Quantity)

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,19 +1,24 @@
-import React, {useState, useEffect, useRef} from 'react';
-import '../css/navCart.css';
-import { useSelector } from 'react-redux';
+import React, { useState } from "react";
+import "../css/navCart.css";
+import { useSelector } from "react-redux";
+import { CartItem } from "./CartItem";
 
-export default function Cart(props){
+export default function Cart(props) {
+    const cartItem = useSelector((state) => state.cart.items);
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = (e) => setIsOpen(!isOpen);
 
-    const [isOpen, setIsOpen] = useState(false);
-    const toggle = (e) => setIsOpen(!isOpen);
-    
-
-
-    return (
-        <div className="cart-wrap">
-            <button className="navCart" onClick={toggle}><div className="navCartIcon"><span className="cartCount">{props.selectedItems}</span></div>
-            </button>
-            <div className={`popup-menu ${isOpen ? 'shown' : ' '}`} ><div>menu</div><button>Pay</button></div>
+  return (
+    <div className="cart-wrap">
+      <button className="navCart" onClick={toggle}>
+        <div className="navCartIcon">
+          <span className="cartCount">{props.selectedItems}</span>
         </div>
-    )
+      </button>
+      <div className={`popup-menu ${isOpen ? "shown" : " "}`}>
+        <div>{cartItem.map(item => <CartItem key={item.item.id} title={item.item.title} price={item.item.price} quantity={item.itemQuantity}/>)}</div>
+        <button>Pay</button>
+      </div>
+    </div>
+  );
 }
