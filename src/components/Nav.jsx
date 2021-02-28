17240d24201677c3a51b2567f348a706127bbbc6
Samir Aboutaka <43029436+Samiiroo@users.noreply.github.com>
2021-02-28 01:28:47 +0100

    Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/nav.css';
+import { Link } from 'react-router-dom';
+import NavItems from './navItems';
 
-
-export default function Nav(){
-    return (
-        <div>
-            <button className="nav"><div className="navIcon"></div></button>
-        </div>
-    )
+export default function Nav() {
+	const [active, setActive] = useState(false);
+	const handleClick = () => setActive(!active);
+	return (
+		<nav className="navBar">
+			<div className="">
+				<button type="button " className="nav" onClick={handleClick}>
+					<div className={!active ? 'navIcon' : 'close'}></div>
+				</button>
+				<div className={!active ? 'navContent' : 'navContentActive'}>
+					{NavItems.map((items, index) => {
+						return (
+							<>
+								<Link className="linkItem" to={items.url} key={index}>
+									{items.item}
+								</Link>
+								<div className="underline"></div>
+							</>
+						);
+					})}
+					<button type="button " className="nav" onClick={handleClick}>
+						<div className={!active ? 'navIcon' : 'close'}></div>
+					</button>
+				</div>
+			</div>
+		</nav>
+	);
 }
